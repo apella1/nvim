f4fe4d697d63cb066561569fd2c04f2aa2a28de8
John Apella <johncollons5@gmail.com>
2025-07-17 09:55:56 +0300

    update init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,50 +1,6 @@
 -- Each plugin specification
 return {
     {
-        "Mofiqul/dracula.nvim",
-        lazy = false,    -- Load immediately
-        priority = 1000, -- Ensure it loads first
-        config = function()
-            local dracula = require("dracula")
-            dracula.setup({
-                colors = {
-                    bg = "#282A36",
-                    fg = "#F8F8F2",
-                    selection = "#44475A",
-                    comment = "#6272A4",
-                    red = "#FF5555",
-                    orange = "#FFB86C",
-                    yellow = "#F1FA8C",
-                    green = "#50fa7b",
-                    purple = "#BD93F9",
-                    cyan = "#8BE9FD",
-                    pink = "#FF79C6",
-                    bright_red = "#FF6E6E",
-                    bright_green = "#69FF94",
-                    bright_yellow = "#FFFFA5",
-                    bright_blue = "#D6ACFF",
-                    bright_magenta = "#FF92DF",
-                    bright_cyan = "#A4FFFF",
-                    bright_white = "#FFFFFF",
-                    menu = "#21222C",
-                    visual = "#3E4452",
-                    gutter_fg = "#4B5263",
-                    nontext = "#3B4048",
-                    white = "#ABB2BF",
-                    black = "#191A21",
-                },
-                show_end_of_buffer = true,
-                transparent_bg = false,
-                lualine_bg_color = "#44475a",
-                italic_comment = true,
-                overrides = {},
-            })
-
-            -- Set the colorscheme
-            vim.cmd([[colorscheme dracula]])
-        end,
-    },
-    {
         "nvim-tree/nvim-web-devicons",
         lazy = false,
     },
@@ -52,12 +8,11 @@ return {
         "nvim-lualine/lualine.nvim",
         dependencies = {
             "nvim-tree/nvim-web-devicons",
-            "Mofiqul/dracula.nvim"
         },
         config = function()
             require("lualine").setup({
                 options = {
-                    theme = 'dracula'
+                    theme = 'auto'
                 }
             })
         end,
